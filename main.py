8ff7d802131e1d0495b4c8ec045befae51085b75
MeninoMirin <tiagodearvalho19041984@gmail.com>
2024-08-27 19:26:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,116 +1,103 @@
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome import service
+from webdriver_manager.opera import OperaDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from time import sleep
-import subprocess
-import platform
-import logging
-import json
-import gc
+#from senhas import *
+
+def verificar_estrategia(lista):
+    for numero in lista[:4]:
+        if numero >= 2:
+            return False
+    return True
+
+webdriver_service = service.Service(OperaDriverManager().install())
+webdriver_service.start()
+options = webdriver.ChromeOptions()
+#options.add_argument('--headless')
+options.add_argument('--disable-logging')
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--start-maximized")
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-infobars')
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_experimental_option('w3c', True)
+
+driver = webdriver.Remote(webdriver_service.service_url,options=options)
+
+driver.maximize_window()
+
+driver.get('https://estrelabet.com/ptb/bet/main')
 
 
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+sleep(10)
+print('Atualizando site...')
+driver.refresh()
 
-class Main:
+print('Aguardando botão para aceitar cookies...')
 
-    def carregar_valores(self):
-        self.ultimos = []
-        with open('config.json') as configFile:
-            config = json.load(configFile)
-            self.quantidade_rodadas = config['quantidade_rodadas']
-            self.mutiplicador_ausente = config['mutiplicador_ausente']
-            self.telegram.start(config['grupos'])
-        
-    def start(self):         
-        system = platform.system()
-        if system == "Windows":
-            subprocess.Popen('taskkill /IM chrome.exe /F', shell=True)
-            sleep(1)
-            subprocess.Popen(
-                f'"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --log-level=3 --remote-debugging-port=9222 --disable-popup-blocking', shell=True)
-        elif system == "Linux":
-            subprocess.Popen('pkill chrome', shell=True)
-            sleep(1)
-            subprocess.Popen(
-                f'/usr/bin/google-chrome --log-level=3 --remote-debugging-port=9222', shell=True)
-        
-        service = ChromeService(executable_path=ChromeDriverManager().install())
-        self.options = webdriver.ChromeOptions()
+WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH,'//*[@id="cookies-bottom-modal"]/div/div[1]/a'))
+    )                                           
 
-        self.options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        self.options.add_argument("--start-maximized")
-        self.options.add_argument("disable-infobars")
-        self.options.add_argument("--disable-extensions")
-        self.options.add_argument("--no-sandbox")
-        
-        sleep(7)
-        self.driver = webdriver.Chrome(service=service, options=self.options)
-        self.config = json.load(open('config.json'))
-        self.driver.get('https://br.parimatch.com/')
-        sleep(10)
-        self.entrando_jogo()
+sleep(10)
 
-    def entrando_jogo(self):        
-        self.driver.get('https://br.parimatch.com/pt/casino/instant-games/game/spribe-br-aviator-inst')
-        sleep(10)
-        iframe = self.driver.find_elements(By.CSS_SELECTOR, 'iframe[title="Aviator"]')[0]
-        self.driver.switch_to.frame(iframe)
-        sleep(5)
+driver.find_element(By.XPATH,'//*[@id="cookies-bottom-modal"]/div/div[1]/a').click()
+sleep(3)
 
-    def ultimos_resultados(self):
-        esperar = 0
-        while True:
-            results = self.driver.find_elements(By.CSS_SELECTOR, 'div.payouts-block > app-bubble-multiplier')
-            ultimos = []
-            esperar += 1
-            if esperar > 60: raise Exception('ultimos_resultados -> tempo esgotado')
-            for result in results:
-                try:
-                    ultimos.append(float(result.text.replace('x', '').strip()))
-                except Exception as e:
-                    logging.error(f"Erro ao processar resultado: {e}")
-            if len(ultimos): return ultimos
+print('Botão clicado')
+print('iniciando login...')
+driver.find_element(By.XPATH,'//*[@id="username"]').send_keys(mario@tmpeml.com)
+print('Usuário')
+sleep(3)
+driver.find_element(By.XPATH,'//*[@id="password-login"]').send_keys(Aviator102030!)
+print('Senha')
+sleep(3)
+driver.find_element(By.XPATH,'//*[@id="header"]/div/div[1]/div/div[2]/app-login/form/div/div/div[2]/button').click()
+print('botão de acesso clicado')
+sleep(5)
 
-    def buscar_alerta(self):
-        if not self.ultimos: return True  # Se `self.ultimos` estiver vazio, sempre alerta
-        for numero in self.ultimos:
-            if numero >= self.mutiplicador_ausente: return False
-        return True
+print('Acessando aviator...')
+driver.get('https://estrelabet.com/ptb/games/detail/casino/normal/7787')
+print('Aguardando Frame...')
 
-    def reset(self):
-        if self.driver: self.driver.quit()
-        sleep(1)
-        del self.driver
-        gc.collect()
-        self.start()
+WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.ID,'gm-frm'))
+    )
+sleep(5)
 
-    def main(self):
-        self.carregar_valores()
-        self.start()
-        while True:
-            try:
-                ultimos = self.ultimos_resultados()
+iframe = driver.find_element(By.ID,'gm-frm')
 
-                if self.ultimos[0:10] == ultimos[0:10]:
-                    continue
+driver.switch_to.frame(iframe)
 
-                if not self.ultimos: self.ultimos = [ultimo for ultimo in ultimos]
-                else: self.ultimos.insert(0, ultimos[0])
+sleep(5)
 
-                while len(self.ultimos) > self.quantidade_rodadas:
-                    self.ultimos.pop()
+while True: 
+    resultados =  [float(n) for n in driver.find_element(
+        By.CLASS_NAME,'result-history').text.replace(
+            'x','').split('\n')][:10]
+    if verificar_estrategia(resultados):
+        print(f'estrategia ok -> {resultados[:4]}')
+    print(resultados)
+    while True:
+        verificacao =  [float(n) for n in driver.find_element(
+            By.CLASS_NAME,'result-history').text.replace(
+                'x','').split('\n')][:10]
+        if verificacao != resultados:
+            break
 
-                if self.buscar_alerta():
-                    self.telegram.send_alert(self.quantidade_rodadas, self.mutiplicador_ausente)
-                    self.ultimos = self.ultimos[0]
-                               
-            except Exception as error:
-                logging.error(f"Erro: {error}", exc_info=True)
-                self.reset()
 
-if __name__ == '__main__':
-    main = Main()
-    main.main()
+
+
+
+
+
+bot estrela_bet.py
+Exibindo bot estrela_bet.py…
